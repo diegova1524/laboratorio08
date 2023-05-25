cd1ad900d0f6186fb65bcda3ccd5e07095d76848
diegova1524 <113550928+diegova1524@users.noreply.github.com>
2023-05-25 12:09:00 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,17 +1,6 @@
-<?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "crud";
-
-try {
-	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-	);
-} catch (Exception $e) {
-	echo "Problema con la conexion: ".$e->getMessage();
-}
-?>
+username = doadmin
+password = AVNS_MnLe_2zYyAPsnYajf9r
+host = db-mysql-nyc1-60144-do-user-14089120-0.b.db.ondigitalocean.com
+port = 25060
+database = defaultdb
+sslmode = REQUIRED
