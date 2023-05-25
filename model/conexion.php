882b1deaa97a596e2d5338c5bc752fc7476d06a1
diegova1524 <113550928+diegova1524@users.noreply.github.com>
2023-05-25 12:29:29 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,6 +1,18 @@
-username = doadmin
+<?php 
+$contrasena = "";
+$usuario = "root";
+$nombre_bd = "crud";
+
+try {
+	$bd = new PDO (
+		username = doadmin
 password = AVNS_MnLe_2zYyAPsnYajf9r
 host = db-mysql-nyc1-60144-do-user-14089120-0.b.db.ondigitalocean.com
 port = 25060
 database = defaultdb
 sslmode = REQUIRED
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+?>
